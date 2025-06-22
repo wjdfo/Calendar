a2b92e5615e661ce61bf46404d382334c8911a0c
kookso <117020362+kookso@users.noreply.github.com>
2025-06-23 02:07:38 +0900

    Feat - Calendar : Allow the access from api without Authentication - GET Method

--- backend/src/main/java/com/example/calendar/calendars/config/SecurityConfig.java
+++ backend/src/main/java/com/example/calendar/calendars/config/SecurityConfig.java
@@ -1,10 +1,11 @@
-package com.example.calendar.config;
+package com.example.calendar.calendars.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity // 이거 꼭 추가해야 Spring Security 설정 적용됨!
@@ -13,10 +14,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())  // 최신 방식으로 변경
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()   // GET 허용
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()  //POST 허용
+                        .anyRequest().permitAll()
                 )
                 .build();
     }
