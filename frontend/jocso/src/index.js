1654385604efee09269d6504d45447d704111915
jungrae <wjdfoek3@knu.ac.kr>
2025-07-06 16:47:34 +0900

    Feat - Auth: page routing

--- frontend/jocso/src/index.js
+++ frontend/jocso/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AppRouter from './router/router';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   </React.StrictMode>
 );
 
